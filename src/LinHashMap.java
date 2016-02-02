c541b2d8c4f3a164c905df99526ca29e0d724e5e
austinmatthews <ausmatt14@gmail.com>
2016-02-02 12:06:03 -0500

    Finished get

--- src/LinHashMap.java
+++ src/LinHashMap.java
@@ -118,10 +118,30 @@ public class LinHashMap <K, V>
      */
     public V get (Object key)
     {
+    	//Gives index of where the key is
         int i = h (key);
-
-        //  T O   B E   I M P L E M E N T E D
-
+        //Gets the index if the original bucket has been split already
+        if(i < split){
+        	i = h2(key);
+        }
+        //Get the bucket in hTable at index i
+        Bucket temp = hTable.get(i);
+        
+        //If the bucket does not have any keys than there can be no values so return null
+        if (temp.nKeys == 0){
+        	return null;
+        }
+        //Loops through each each array in the bucket
+        while(temp != null){
+    		//Loop through each index of the current array in the bucket
+    		for(int h = 0; h < SLOTS; h++){
+    			//If the key equals the key at the current index, return the value at the current index
+    			if(key == temp.key[h]){
+    				return temp.value[h];
+    			}
+    		}
+    		temp = temp.next;
+    	}
         return null;
     } // get
 
